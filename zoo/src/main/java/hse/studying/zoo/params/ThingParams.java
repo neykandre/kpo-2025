1877ed17c63f99c55d2268ce947a201f5daf44fd
neykandre <andrejnosov.01.02.05@gmail.com>
2025-02-14 06:24:16 +0300

    add hw-directory, rearranged zoo-directory
    # Conflicts:
    #	hw/zoo/build.gradle.kts
    #	hw/zoo/src/main/java/hse/studying/zoo/ZooApplication.java
    #	hw/zoo/src/main/java/hse/studying/zoo/services/Clinic.java
    #	hw/zoo/src/main/java/hse/studying/zoo/services/ConsoleMenu.java

--- zoo/src/main/java/hse/studying/zoo/params/ThingParams.java
+++
@@ -1,9 +0,0 @@
-package hse.studying.zoo.params;
-
-/**
- * Represents the parameters for a {@link hse.studying.zoo.domains.Thing}.
- *
- * @param inventoryNumber the inventory number assigned to the thing
- */
-public record ThingParams(int inventoryNumber) {
-}
