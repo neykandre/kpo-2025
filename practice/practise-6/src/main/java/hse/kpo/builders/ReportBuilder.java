8cc52b11da5ffea7f9b6ed21d86b7421926b22e7
neykandre <andrejnosov.01.02.05@gmail.com>
2025-04-20 16:43:41 +0300

    Revert "cw6"
    This reverts commit 07eb5a20a7c313f857ac1dcd02ef1866c90659b4.

--- practice/practise-6/src/main/java/hse/kpo/builders/ReportBuilder.java
+++ practice/practise-6/src/main/java/hse/kpo/builders/ReportBuilder.java
@@ -49,13 +49,6 @@ public class ReportBuilder {
         return this;
     }
 
-    public ReportBuilder addCatamarans(List<Catamaran> catamarans) {
-        content.append("Катамараны:");
-        catamarans.forEach(catamaran -> content.append(String.format(" - %s", catamaran)));
-        content.append("\n");
-        return this;
-    }
-
     /**
      * Метод получения итогового отчета о системе.
      *
