b737356f0f6584281b7593d947713576c7b992d4
neykandre <andrejnosov.01.02.05@gmail.com>
2025-04-20 16:34:29 +0300

    init

---
+++ hw/zoo2/src/test/java/hse/studying/zoo2/Zoo2ApplicationTests.java
@@ -0,0 +1,12 @@
+package hse.studying.zoo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zoo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
